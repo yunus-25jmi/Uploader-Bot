8f718012fcdb405734aa35f82308b24536446f4a
Tellybots <93035329+Tellybots@users.noreply.github.com>
2022-02-08 13:48:55 +0530

    Create button.py

--- plugins/button.py
+++ plugins/button.py
@@ -21,7 +21,7 @@ from plugins.functions.display_progress import progress_for_pyrogram, humanbytes
 # https://stackoverflow.com/a/37631799/4723940
 
 from PIL import Image
-from .functions.ran_text import random_char
+from plugins.functions.ran_text import random_char
 
 async def youtube_dl_call_back(bot, update):
     cb_data = update.data
