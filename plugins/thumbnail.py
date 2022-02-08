31b6a55ccacd2e7b3799fa122d5971ce3945b7e1
Tellybots <93035329+Tellybots@users.noreply.github.com>
2022-02-08 14:07:01 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -22,3 +22,90 @@ from .database.database import db
 from .functions.help_Nekmo_ffmpeg import take_screen_shot
 
 
+@Client.on_message(filters.private & filters.photo)
+async def save_photo(bot, update):
+    
+    await db.set_thumbnail(update.from_user.id, thumbnail=update.photo.file_id)
+    await bot.send_message(chat_id=update.chat.id, text=Translation.SAVED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
+
+@Client.on_message(filters.private & filters.command("delthumbnail"))
+async def delthumbnail(bot, update):
+    
+    await db.set_thumbnail(update.from_user.id, thumbnail=None)
+    await bot.send_message(chat_id=update.chat.id, text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
+
+@Client.on_message(filters.private & filters.command("showthumb") )
+async def viewthumbnail(bot, update):
+    
+    thumbnail = await db.get_thumbnail(update.from_user.id)
+    if thumbnail is not None:
+        await bot.send_photo(
+        chat_id=update.chat.id,
+        photo=thumbnail,
+        caption=f"Your current saved thumbnail 🦠",
+        reply_to_message_id=update.message_id)
+    else:
+        await update.reply_text(text=f"No Thumbnail found 🤒")
+
+async def Gthumb01(bot, update):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    db_thumbnail = await db.get_thumbnail(update.from_user.id)
+    if db_thumbnail is not None:
+        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+        Image.open(thumbnail).convert("RGB").save(thumbnail)
+        img = Image.open(thumbnail)
+        img.resize((100, 100))
+        img.save(thumbnail, "JPEG")
+    else:
+        thumbnail = None
+
+    return thumbnail
+
+async def Gthumb02(bot, update, duration, download_directory):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    db_thumbnail = await db.get_thumbnail(update.from_user.id)
+    if db_thumbnail is not None:
+        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+    else:
+        thumbnail = await take_screen_shot(download_directory, os.path.dirname(download_directory), random.randint(0, duration - 1))
+
+    return thumbnail
+
+async def Mdata01(download_directory):
+
+          width = 0
+          height = 0
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+              if metadata.has("width"):
+                  width = metadata.get("width")
+              if metadata.has("height"):
+                  height = metadata.get("height")
+
+          return width, height, duration
+
+async def Mdata02(download_directory):
+
+          width = 0
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+              if metadata.has("width"):
+                  width = metadata.get("width")
+
+          return width, duration
+
+async def Mdata03(download_directory):
+
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+
+          return duration
