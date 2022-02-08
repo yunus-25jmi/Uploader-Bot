e8830ce77ed45981e6ecb6bac43a554eaca5ebd6
Tellybots <93035329+Tellybots@users.noreply.github.com>
2022-02-08 16:05:26 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -13,7 +13,7 @@ import time
 # the Strings used for this "thing"
 from plugins.main import Translation
 from pyrogram import Client
-
+from plugins.config import Config
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
