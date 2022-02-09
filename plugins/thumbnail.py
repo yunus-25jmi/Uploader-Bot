acc642991eece130eac4880ec82112baa8738ad3
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-09 16:43:42 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -1,4 +1,4 @@
-
+# Copyright @Tellybots | @ShriMadhavUk
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
