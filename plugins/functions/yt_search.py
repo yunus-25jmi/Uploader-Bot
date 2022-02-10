f4c8350812e6f94a83132395c9d23f0c03c60ed7
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-10 12:43:13 +0530

    Create yt_search.py

---
+++ plugins/functions/yt_search.py
@@ -0,0 +1,34 @@
+from pyrogram import Client ,filters
+import os
+from py_youtube import Data, Search 
+from pyrogram.types import *
+
+ 
+	
+
+
+@Client.on_inline_query()
+async def search_video(client,query):
+	search = []
+	result = Search(query.query.strip()).videos()
+	for i in result:
+		try:
+			title = i["title"]
+			id = i["id"]
+			thumb = i["thumb"][0]
+			data = i["simple_data"]
+		except:
+			pass
+		try:
+			search.append(
+                InlineQueryResultPhoto(
+                    title=title,
+                    description=data,
+                    caption="https://youtu.be/"+id))
+		
+		except:
+		          pass
+            
+	await query.answer(search)
+	
+
