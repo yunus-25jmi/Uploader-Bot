51507d893d6ef675621973330316b8620293ffac
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-10 11:59:56 +0530

    Create yt_search.py

--- plugins/functions/yt_search.py
+++ plugins/functions/yt_search.py
@@ -7,3 +7,30 @@ from pyrogram.types import *
 
 
 @Client.on_inline_query()
+async def start(client,message):
+	await message.reply_text("Helo iam Youtube Video Search\nUse in inline mode")
+	
+
+
+@app.on_inline_query()
+async def search_video(client,query):
+	search = []
+	result = Search(query.query.strip()).videos()
+	for i in result:
+		try:
+			title = i["title"]
+			id = i["id"]
+			data = i["simple_data"]
+		except:
+			pass
+		try:
+			search.append(
+                InlineQueryResultPhoto(
+                    title=title,
+                    description=data,
+                    caption="https://youtu.be/"+id))
+		
+		except:
+		          pass
+            
+	await query.answer(search)
