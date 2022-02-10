8291398766af65333a13967269fe90e29599e50b
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-10 11:57:35 +0530

    Update yt_search.py

--- plugins/functions/yt_search.py
+++ plugins/functions/yt_search.py
@@ -7,30 +7,3 @@ from pyrogram.types import *
 
 
 @Client.on_inline_query()
-async def search_video(client,query):
-	search = []
-	result = Search(query.query.strip()).videos()
-	for i in result:
-		try:
-			title = i["title"]
-			id = i["id"]
-			data = i["simple_data"]
-		except:
-			pass
-		
-            
-	await query.answer(search)
-	
-
-			pass
-		try:
-			search.append(
-                InlineQueryResultPhoto(
-                    title=title,
-                    description=data,
-                    caption="https://youtu.be/"+id))
-		
-		except:
-		          pass
-            
-	await query.answer(search)
