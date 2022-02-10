c573e7242835d2c02c91819aaf49292e1497abf7
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-10 12:08:44 +0530

    Update yt_search.py

--- plugins/functions/yt_search.py
+++ plugins/functions/yt_search.py
@@ -10,24 +10,41 @@ from pyrogram.types import *
 
 
 @Client.on_inline_query()
-async def search_video(client,query):
-	search = []
-	result = Search(query.query.strip()).videos()
-	for i in result:
-		try:
-			title = i["title"]
-			id = i["id"]
-			data = i["simple_data"]
-		except:
-			pass
-		try:
-			search.append(
+async def search(bot, update):   
+    results = VideosSearch(update.query, limit=50).result()
+    answers = []    
+    for result in results:
+        title = result["title"]
+        views_short = result["viewCount"]["short"]
+        duration = result["duration"]
+        duration_text = result["accessibility"]["duration"]
+        views = result["viewCount"]["text"]
+        publishedtime = result["publishedTime"]
+        channel_name = result["channel"]["name"]
+        channel_link = result["channel"]["link"]
+        description = f"{views_short} | {duration}"
+        details = f"**Title:** {title}" + "\n" \
+        f"**Channel:** [{channel_name}]({channel_link})" + "\n" \
+        f"**Duration:** {duration_text}" + "\n" \
+        f"**Views:** {views}" + "\n" \
+        f"**Published Time:** {publishedtime}" + "\n" \
+        "\n" + "**Made by @FayasNoushad**"
+        thumbnail = ytthumb.thumbnail(result["id"])
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Watch Video 📹", url=result["link"])]]
+        )
+        try:
+            answers.append(
                 InlineQueryResultPhoto(
                     title=title,
-                    description=data,
-                    caption="https://youtu.be/"+id))
-		
-		except:
-		          pass
-            
-	await query.answer(search)
+                    description=description,
+                    caption=details,
+                    photo_url=thumbnail,
+                    reply_markup=reply_markup
+                )
+            )
+        except:
+            pass
+    
+    await update.answer(answers)
+
