0b894d5fd481fed5959d9b313c83c05542cebf2d
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-10 13:11:05 +0530

    Update yt_search.py

--- plugins/functions/yt_search.py
+++ plugins/functions/yt_search.py
@@ -1,34 +1,69 @@
-from pyrogram import Client ,filters
-import os
-from py_youtube import Data, Search 
-from pyrogram.types import *
 
- 
 	
+
+import os
+import asyncio
+from pyrogram import Client
+from functions.presets import Presets
+from functions.sqldb import add_user
+from functions.defaults import get_info
+from pyrogram.errors import FloodWait
+from functions.extract import youtube_search
+from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+
+
+from plugins.config import Config
 
 
 @Client.on_inline_query()
-async def search_video(client,query):
-	search = []
-	result = Search(query.query.strip()).videos()
-	for i in result:
-		try:
-			title = i["title"]
-			id = i["id"]
-			thumb = i["thumb"][0]
-			data = i["simple_data"]
-		except:
-			pass
-		try:
-			search.append(
-                InlineQueryResultPhoto(
-                    title=title,
-                    caption="https://youtu.be/"+id,
-                    photo_url=thumb))
-
-		except:
-		          pass
-            
-	await query.answer(search)
-	
-
+async def inline_search(bot, query: InlineQuery):
+    await add_user(query.from_user.id)
+    me = []
+    try:
+        me = await Client.get_me(bot)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+    id = query.from_user.id
+    results = []
+    #
+    defaults = await get_info(me.username)
+    results.extend(defaults)
+    #
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+    #
+    search = query.query.strip()
+    string = await youtube_search(search)
+    for data in string:
+        count = data['viewCount']
+        thumb = data['thumbnails']
+        results.append(
+            InlineQueryResultArticle(
+                title=data['title'][:35] + "..",
+                input_message_content=InputTextMessageContent(
+                    message_text=data['link']
+                ),
+                thumb_url=thumb[0]['url'],
+                description=Presets.DESCRIPTION.format(data['duration'], count['text'])
+            )
+        )
+    if string:
+        switch_pm_text = Presets.RESULTS_TXT
+        try:
+            await query.answer(
+                results=results,
+                switch_pm_text=switch_pm_text,
+                switch_pm_parameter="start"
+            )
+        except Exception:
+            pass
+    else:
+        switch_pm_text = Presets.NO_RESULTS
+        try:
+            await query.answer(
+                results=results,
+                switch_pm_text=switch_pm_text,
+                switch_pm_parameter="start"
+            )
+        except Exception:
+            pass
