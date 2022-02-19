81088021f7514d7479561651ecacd372890e36ab
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-19 11:17:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,6 +8,7 @@ from plugins.main import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins.database.add import add_user_to_database
+from .functions.forcesub import handle_force_subscribe
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
