1ae85eb068a886b7fc7474ad8382e23f74baef8b
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-19 08:45:57 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -94,7 +94,7 @@ async def echo(bot, update):
     logger.info(command_to_exec)
     chk = await bot.send_message(
             chat_id=update.chat.id,
-            text=f'Processing Your Link ⌛',
+            text=f'Pʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ʟɪɴᴋ ⌛',
             disable_web_page_preview=True,
             reply_to_message_id=update.message_id
           )
