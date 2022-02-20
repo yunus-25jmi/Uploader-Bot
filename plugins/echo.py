93f25a58d9dea3c53fbf3145ee0f06da10703116
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-20 13:35:18 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -58,7 +58,7 @@ async def echo(bot, update):
         except Exception as error:
             print(error)
     logger.info(update.from_user.id)
-    fmsg = await update.reply_text(text=Translation.CHECKING_LINK, quote=True)
+    chk = await update.reply_text(text=Translation.CHECKING_LINK, quote=True)
     url = update.text
 
     youtube_dl_username = None
@@ -262,6 +262,62 @@ async def echo(bot, update):
             ])
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
 
+        await chk.delete(True)
+        thumbnail = Config.DEF_THUMB_NAIL_VID_S
+        thumbnail_image = Config.DEF_THUMB_NAIL_VID_S
+        if "thumbnail" in response_json:
+            if response_json["thumbnail"] is not None:
+                thumbnail = response_json["thumbnail"]
+                thumbnail_image = response_json["thumbnail"]
+        thumb_image_path = DownLoadFile(
+            thumbnail_image,
+            Config.DOWNLOAD_LOCATION + "/" +
+            str(update.from_user.id) + ".webp",
+            Config.CHUNK_SIZE,
+            None,  # bot,
+            Translation.DOWNLOAD_START,
+            update.message_id,
+            update.chat.id
+        )
+        if os.path.exists(thumb_image_path):
+            im = Image.open(thumb_image_path).convert("RGB")
+            im.save(thumb_image_path.replace(".webp", ".jpg"), "jpeg")
+        else:
+            thumb_image_path = None
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=update.message_id
+        )
+    else:
+        # fallback for nonnumeric port a.k.a seedbox.io
+        inline_keyboard = []
+        cb_string_file = "{}={}={}".format(
+            "file", "LFO", "NONE")
+        cb_string_video = "{}={}={}".format(
+            "video", "OFL", "ENON")
+        inline_keyboard.append([
+            InlineKeyboardButton(
+                "SVideo",
+                callback_data=(cb_string_video).encode("UTF-8")
+            ),
+            InlineKeyboardButton(
+                "DFile",
+                callback_data=(cb_string_file).encode("UTF-8")
+            )
+        ])
+        reply_markup = InlineKeyboardMarkup(inline_keyboard)
+        await chk.delete(True)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.FORMAT_SELECTION.format(""),
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=update.message_id
+        )
+            
 
 
                 
