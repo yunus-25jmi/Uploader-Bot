bea173b0da0b758b3892179a94c1d464d4cf168e
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-21 12:52:12 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -23,76 +23,26 @@ from .functions.ran_text import random_char
 from .functions.forcesub import handle_force_subscribe
 
 @Client.on_message(filters.private & filters.regex(pattern=".*https.*"))
-async def echo(bot, update):
-    await add_user_to_database(bot, update)
-    logger.info(update.from_user)
-    if Config.UPDATES_CHANNEL:
-      fsub = await handle_force_subscribe(bot, update)
-      if fsub == 400:
-        return
-    url = update.text
-    youtube_dl_username = None
-    youtube_dl_password = None
-    file_name = None
-    print(url)
-    if "|" in url:
-        url_parts = url.split("|")
-        if len(url_parts) == 2:
-            url = url_parts[0]
-            file_name = url_parts[1]
-        elif len(url_parts) == 4:
-            url = url_parts[0]
-            file_name = url_parts[1]
-            youtube_dl_username = url_parts[2]
-            youtube_dl_password = url_parts[3]
-        else:
-            for entity in update.entities:
-                if entity.type == "text_link":
-                    url = entity.url
-                elif entity.type == "url":
-                    o = entity.offset
-                    l = entity.length
-                    url = url[o:o + l]
-        if url is not None:
-            url = url.strip()
-        if file_name is not None:
-            file_name = file_name.strip()
-        # https://stackoverflow.com/a/761825/4723940
-        if youtube_dl_username is not None:
-            youtube_dl_username = youtube_dl_username.strip()
-        if youtube_dl_password is not None:
-            youtube_dl_password = youtube_dl_password.strip()
-        logger.info(url)
-        logger.info(file_name)
-    else:
-        for entity in update.entities:
-            if entity.type == "text_link":
-                url = entity.url
-            elif entity.type == "url":
-                o = entity.offset
-                l = entity.length
-                url = url[o:o + l]
-    if Config.HTTP_PROXY != "":
-        command_to_exec = [
+async def echo(url, cf_name, yt_dl_user_name, yt_dl_pass_word, user_working_dir):
+    command_to_exec = [
         "yt-dlp",
         "--no-warnings",
         "--youtube-skip-dash-manifest",
-        "--no-check-certificate",
         "-j",
-        url,
+        url
     ]
     if "hotstar" in url:
         command_to_exec.append("--geo-bypass-country")
         command_to_exec.append("IN")
     #
-    if youtube_dl_username is not None:
+    if yt_dl_user_name is not None:
         command_to_exec.append("--username")
         command_to_exec.append(yt_dl_user_name)
-    if youtube_dl_password is not None:
+    if yt_dl_pass_word is not None:
         command_to_exec.append("--password")
         command_to_exec.append(yt_dl_pass_word)
 
- 
+    LOGGER.info(command_to_exec)
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
         # stdout must a pipe to be accessible as process.stdout
@@ -102,9 +52,9 @@ async def echo(bot, update):
     # Wait for the subprocess to finish
     stdout, stderr = await process.communicate()
     e_response = stderr.decode().strip()
-    logger.info(e_response)
+    LOGGER.info(e_response)
     t_response = stdout.decode().strip()
-    # LOGGER.info(t_response)
+    LOGGER.info(t_response)
     # https://github.com/rg3/youtube-dl/issues/2630#issuecomment-38635239
     if e_response:
         # logger.warn("Status : FAIL", exc.returncode, exc.output)
